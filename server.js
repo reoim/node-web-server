baafc55442a46f34bf706e6c69558b72ca6bf96d
Reo Dongmin Lee <reoim10@gmail.com>
2017-07-17 20:01:09 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const hbs = require('hbs');
+
+let app = express()
+
+app.set('view-engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!!</h1>')
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        name: 'Reo',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear(),
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
