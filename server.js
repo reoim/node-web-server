d97558712db82c752afadfbb2c2311c0fda800c5
Reo Dongmin Lee <reoim10@gmail.com>
2017-07-17 20:35:04 +0900

    Add express middleware

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require('express')
-const hbs = require('hbs');
+const hbs = require('hbs')
+const fs = require('fs')
 
 let app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view-engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+app.use((req, res, next) => {
+    let now = new Date().toString()
+    let log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    })
+    next()
+})
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+})
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!!</h1>')
     res.render('home.hbs', {
         pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
         name: 'Reo',
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear(),
+        pageTitle: 'About Page'
     })
 })
 
